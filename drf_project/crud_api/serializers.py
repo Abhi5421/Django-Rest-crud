69a76bac39d8632f75d8fd2f5c57b1885fd0c892
Abhijith <abhijith.kp@reward360.co>
2024-03-15 18:11:16 +0530

    added validations to models

--- drf_project/crud_api/serializers.py
+++ drf_project/crud_api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Item
+from rest_framework.exceptions import ValidationError
 
 
 class ItemSerializer(serializers.ModelSerializer):
@@ -7,3 +8,9 @@ class ItemSerializer(serializers.ModelSerializer):
         model = Item
         fields = ('category', 'subcategory', 'name', 'amount')
         # fields = '__all__'
+
+    @staticmethod
+    def validate_amount(data):
+        if data <= 0:
+            raise ValidationError("Amount must be greater than zero.")
+        return data
